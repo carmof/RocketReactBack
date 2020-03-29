3cc34d6e961ccfd39f19ea1c40f5050d0d93491d
Felipe Carmo <felipe.carmo@mpscloud.com.br>
2020-03-29 17:01:53 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+    console.log('hello')
+    res.status(200).json({ msg: 'Felipe' })
+})
+app.listen(3333)
